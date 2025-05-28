9a5ede694765ff48f9e5f6f4aa3b0d320603f237
Nadhif Adita <nadijfer02@gmail.com>
2025-05-28 08:43:04 +0700

    ertyuiop

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Halo Laravel</title>
+    @vite('resources/css/app.css') {{-- untuk tailwind jika dipakai --}}
+</head>
+<body class="bg-gray-100 text-center">
+    <h1 class="text-4xl font-bold text-blue-600 mt-10">Halo dari Laravel!</h1>
+</body>
+</html>
