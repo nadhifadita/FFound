04cc659b40ed39eb94d337ec08e74386f5476e30
Andikasaktih <andikasaktih@gmail.com>
2025-05-29 13:43:46 +0700

    add dashboard and components in view

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard_login', function () {
+    return view('dashboard.dashboard_login');
+});
+Route::get('/dashboard_logout', function () {
+    return view('dashboard.dashboard_logout');
+});
+Route::get('/dashboard_login_satpam', function () {
+    return view('dashboard.dashboard_login_satpam');
+});
