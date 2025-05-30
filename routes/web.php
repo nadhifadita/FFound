0d4f655681782ec983834300b54b656c6ac20c11
faqihabyan <faqihabyan7@gmail.com>
2025-05-30 13:55:48 +0700

    add login register forgorpass

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard.dashboard_logout');
 });
 Route::get('/dashboard_login', function () {
     return view('dashboard.dashboard_login');
@@ -13,4 +13,13 @@ Route::get('/dashboard_logout', function () {
 });
 Route::get('/dashboard_login_satpam', function () {
     return view('dashboard.dashboard_login_satpam');
+});
+Route::get('/login', function () {
+    return view('autentikasi.login');
+})->name('login');
+Route::get('/register', function () {
+    return view('autentikasi.register');
+})->name('register');
+Route::get('/forgot_password', function () {
+    return view('autentikasi.forgotPassword');
 });
